8465cff6c9aafd65266c2f2f9e182d789033641a
Xinbin Huang <bin.huangxb@gmail.com>
2020-05-19 13:50:50 -0700

    Update config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,18 @@
 export const config = {
-  "dev": {
+  "db": {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DBNAME,
     "host": process.env.HOST,
-    "dialect": process.env.DIALECT,
+    "dialect": process.env.DIALECT
+  },
+  "aws": {
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  "jwt": {
+    "secret": process.env.JWT_SECRET
   }
+}
 }
